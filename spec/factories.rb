e22320f1865bcfa2d389c7c48afb5d69e6e8c76b
Austin Sprawls <austinsprawls@gmail.com>
2014-06-08 17:08:59 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name  "Austin Sprawls"
+        email "asprawls@example.com"
+        password "foobar"
+        password_confirmation "foobar"
+    end
+end
